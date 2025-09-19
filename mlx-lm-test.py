a3f3d35f5f3442faca8974b8b3acc96aff35b92a
Cornell STAP <lukeshao2022@gmail.com>
2025-09-19 17:32:49 -0400

    Working to setup model finetuning - Using mlx-lm-lora library

--- mlx-lm-test.py
+++ mlx-lm-test.py
@@ -1,12 +1,28 @@
 from mlx_lm import load, generate
+from pathlib import Path
+import json
 
 model, tokenizer = load("mlx-community/Meta-Llama-3-8B-Instruct")
-
 prompt = "Write python code to compute the fibonacci sequence."
 
 messages = [{"role": "user", "content": prompt}]
 prompt = tokenizer.apply_chat_template(
     messages, add_generation_prompt=True
 )
+
+adapter_path = Path("adapters")
+adapter_path.mkdir(parents=True, exist_ok=True)
+
+lora_config = {
+    "num_layers" : 8,
+    "lora_parameters" : {
+        "rank" : 8,
+        "scale" : 20.0,
+        "dropout" : 0.05,
+    }
+}
+
+with open(adapter_path / "adapter_config.json", "w") as fid:
+    json.dump(lora_config, fid, indent=4)
 
 text = generate(model, tokenizer, prompt=prompt, verbose=True)
